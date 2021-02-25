92777b4c71c265b3ca2c6530fc1430a03f1eaea1
jhosep98 <jhosep149@hotmail.com>
2021-02-25 10:36:50 -0300

    initial commit
    - webpack and babeljs configuration.
    - using eslint and the Airbnb style guide.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App = () => (
+  <>
+    <CssBaseline />
+    <h1>Hello React!</h1>
+  </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(pdf|png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
